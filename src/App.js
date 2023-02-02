826e653d696f623a165b0c058223b90939d6211e
RomanEvd <evdokimov.roman.1993@mail.ru>
2023-02-02 21:11:34 +0400

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,52 @@
-import logo from './logo.svg';
-import './App.css';
+import { React, useState, useEffect } from "react";
+import List from "./components/List";
+import Details from "./components/Details";
+import "./App.css";
 
 function App() {
+  const [dataList, setData] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const [info, setInfo] = useState(null);
+  const [error, setError] = useState(false);
+  useEffect(() => {
+    setLoading(true);
+    const fetchData = async () => {
+      try {
+        const response = await fetch(
+          `${process.env.REACT_APP_DATA_URL}users.json`
+        );
+        if (!response.ok) {
+          throw new Error(response.statusText);
+        }
+        const dataList = await response.json();
+        setData(dataList);
+      } catch (error) {
+        setError(error);
+      } finally {
+        setLoading(false);
+      }
+    };
+    fetchData();
+  }, []);
+
+  const handlyClick = (id) => {
+    const infoObj = dataList[dataList.findIndex((item) => item.id === id)];
+    setInfo(infoObj);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='containier'>
+      {loading && <p>Загрузка</p>}
+      {error && <p>{error}</p>}
+      <List
+        dataList={dataList}
+        handlyClick={handlyClick}
+        active={info && info.id}
+      />
+      {info ? <Details info={info} /> : null}
     </div>
   );
 }
 
 export default App;
+
